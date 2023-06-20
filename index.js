ca0fbdbd781ee029bbf6596acea254a8d07472d7
Justin Searls <searls@gmail.com>
2023-06-20 08:07:29 -0400

    export a smaller, undocumented method designed to help me (and maybe only me) test the height of SVGs for dynamic text scaling

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const DEFAULT_CONFIG = {
   ]
 }
 
-export default async function (src, dest, options) {
+export default async function realOg (src, dest, options) {
   log(`Writing ${dest} from ${src} markdown`)
   const page = await readMarkdown(src, { summarySeparator: '<!--more-->', ...options })
   log(`Rendering SVG for ${src}`)
@@ -37,4 +37,20 @@ export default async function (src, dest, options) {
   await exportImage(png, dest, { fileType: 'png', ...options })
   log(`Finished writing ${dest}`)
   return dest
+}
+
+/* This is not a function to be proud of.
+ * It will render the markdown to SVG at a particular width and with the user's
+ * supplied template and options, and then it'll run a regex against the
+ * resulting SVG to find the height of the first rect in the output.
+ *
+ * The output currently starts like this, which the regex is based on :
+ *  <svg width="2088" height="100000" viewBox="0 0 2088 100000" xmlns="http://www.w3.org/2000/svg"><mask id="satori_om-id"><rect x="0" y="0" width="2102" height="549" fill="#fff"/></mask>
+ *
+ * Obligatory you can't parse HTML with regex link: https://blog.codinghorror.com/parsing-html-the-cthulhu-way/
+ */
+export async function measureHeight (src, options) {
+  const page = await readMarkdown(src, { summarySeparator: '<!--more-->', ...options })
+  const svg = await renderTemplate(page, Object.assign({}, DEFAULT_CONFIG, options))
+  return Number(svg.match(/<rect[^>]*height="(\d+)"/)?.[1])
 }
